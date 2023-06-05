b731d9ad14039221d860706b6b961486992a3401
mich43ll <michaell@gmail.com>
2023-06-05 16:57:14 -0600

    Status code
    uso de los codigos de status con ActionResult tales como BadRequest, NotFound y Ok

---
+++ MagicVilla_API/Controllers/villaController.cs
@@ -0,0 +1,36 @@
+﻿using MagicVilla_API.Data;
+using MagicVilla_API.Models;
+using MagicVilla_API.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class villaController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult< IEnumerable<villaDto>> GetVillas()
+        {
+            return Ok( VillaStore.villaList);
+        }
+
+        [HttpGet("id")]
+        public ActionResult<villaDto> GetVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(v => v.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(villa);
+        }
+    }
+}
---
+++ MagicVilla_API/Data/VillaStore.cs
@@ -0,0 +1,13 @@
+﻿using MagicVilla_API.Models.Dto;
+
+namespace MagicVilla_API.Data
+{
+    public static class VillaStore
+    {
+        public static List<villaDto> villaList = new List<villaDto>
+        {
+            new villaDto { Id = 1, Nombre= "vista a la playa"},
+            new villaDto { Id = 2, Nombre="Vista a la piscina"}
+        };
+    }
+}
